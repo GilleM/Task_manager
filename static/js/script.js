5a2adae6a93e443d0df8c15644432ed7bcc4c557
GilleM <matea.leka@gmail.com>
2021-04-21 21:16:34 +0000

    Add categories dropdown

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,10 @@
-$(document).ready(function () {
-    $(".sidenav").sidenav({edge: "right"});
+$(document).ready(function() {
+    $(".sidenav").sidenav({
+        edge: "right"
+    });
     $(".collapsible").collapsible();
     $(".tooltipped").tooltip();
+    $('select').formSelect();
     $(".datepicker").datepicker({
         format: "dd mmmm, yyyy",
         yearRange: 3,
@@ -10,4 +13,6 @@ $(document).ready(function () {
             done: "Select"
         }
     });
+
+
 });
